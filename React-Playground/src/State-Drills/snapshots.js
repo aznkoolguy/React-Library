b7a08e19cf5659a9b69bcda941c8eb269468fc27
aznkoolguy <34627946+aznkoolguy@users.noreply.github.com>
2021-06-02 12:17:31 -0500

    Update and rename snapshots to snapshots.js

---
+++ React-Playground/src/State-Drills/snapshots.js
@@ -0,0 +1,120 @@
+// Jest Snapshot v1, https://goo.gl/fbAQLP
+
+exports[`Accordion Component only opens one section at a time 1`] = `
+<ul
+  className="accordions"
+>
+  <li
+    className="accordionButton"
+    key="0"
+  >
+    <button
+      onClick={[Function]}
+      type="button"
+    >
+      Section 1
+    </button>
+  </li>
+  <li
+    className="accordionButton"
+    key="1"
+  >
+    <button
+      onClick={[Function]}
+      type="button"
+    >
+      Section 2
+    </button>
+  </li>
+  <li
+    className="accordionButton"
+    key="2"
+  >
+    <button
+      onClick={[Function]}
+      type="button"
+    >
+      Section 3
+    </button>
+  </li>
+</ul>
+`;
+
+exports[`Accordion Component opens any clicked section 1`] = `
+<ul
+  className="accordions"
+>
+  <li
+    className="accordionButton"
+    key="0"
+  >
+    <button
+      onClick={[Function]}
+      type="button"
+    >
+      Section 1
+    </button>
+  </li>
+  <li
+    className="accordionButton"
+    key="1"
+  >
+    <button
+      onClick={[Function]}
+      type="button"
+    >
+      Section 2
+    </button>
+  </li>
+  <li
+    className="accordionButton"
+    key="2"
+  >
+    <button
+      onClick={[Function]}
+      type="button"
+    >
+      Section 3
+    </button>
+  </li>
+</ul>
+`;
+
+exports[`Accordion Component renders empty given no Accordion without errors 1`] = `
+<ul
+  className="accordions"
+/>
+`;
+
+exports[`Accordion Component renders empty given no tabs 1`] = `
+<ul
+  className="accordions"
+>
+  0
+</ul>
+`;
+
+exports[`Accordion Component renders the no sections by default 1`] = `
+<ul
+  className="accordions"
+>
+  <li
+    className="accordionButton"
+    key="0"
+  >
+    Section 1
+  </li>
+  <li
+    className="accordionButton"
+    key="1"
+  >
+    Section 2
+  </li>
+  <li
+    className="accordionButton"
+    key="2"
+  >
+    Section 3
+  </li>
+</ul>
+`;
