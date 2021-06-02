f5b47742f1331c8d4d9e0696967f392243ac4993
aznkoolguy <34627946+aznkoolguy@users.noreply.github.com>
2021-06-02 13:29:51 -0500

    Create Counter.js

---
+++ React-Playground/src/state/Counter.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+
+class Counter extends React.Component {
+  static defaultProps = {
+    step : 1
+  }
+  constructor(props) {
+    super(props)
+    this.state = { count: 0 }
+  }
+  handleButtonClick = () => {
+    this.setState({ count: this.state.count + this.props.step})
+  }
+  render() {
+    return (
+      <div>
+        <p>The current count: {this.state.count}</p>
+        <button onClick={this.handleButtonClick}>
+          Add 1
+        </button>
+      </div>
+    )
+  }
+}
+
+export default Counter;
